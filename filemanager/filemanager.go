88fdfc5246a740d237aac53dcacf9a220e755b36
hassanjawwad12 <hassanjawwad01@gmail.com>
2025-03-04 14:34:34 +0500

    ✅ Added Concurrency Using Channels

--- filemanager/filemanager.go
+++ filemanager/filemanager.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"errors"
 	"os"
+	"time"
 )
 
 type FileManager struct {
@@ -19,6 +20,8 @@ func (fm FileManager) ReadLines() ([]string, error) {
 		return nil, errors.New("failed to open file")
 	}
 
+	defer file.Close()
+
 	scanner := bufio.NewScanner(file)
 
 	var lines []string
@@ -31,10 +34,9 @@ func (fm FileManager) ReadLines() ([]string, error) {
 
 	if err != nil {
 		file.Close()
-		return nil, errors.New("Failed to read line in file.")
+		return nil, errors.New("failed to read line in file")
 	}
 
-	file.Close()
 	return lines, nil
 }
 
@@ -45,6 +47,10 @@ func (fm FileManager) WriteResult(data interface{}) error {
 		return errors.New("failed to create file")
 	}
 
+	defer file.Close()
+
+	time.Sleep(3 * time.Second)
+
 	encoder := json.NewEncoder(file)
 	err = encoder.Encode(data)
 
@@ -53,7 +59,6 @@ func (fm FileManager) WriteResult(data interface{}) error {
 		return errors.New("faild to convert data to JSON")
 	}
 
-	file.Close()
 	return nil
 }
 
--- main.go
+++ main.go
@@ -11,14 +11,26 @@ import (
 func main() {
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
-	for _, taxRate := range taxRates {
+	// channels equal to tax rates
+	doneChans := make([]chan bool, len(taxRates))
+	errorChans := make([]chan error, len(taxRates))
+
+	for index, taxRate := range taxRates {
+		doneChans[index] = make(chan bool)
+		errorChans[index] = make(chan error)
 		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
 		//cmd := cmdmanager.New()
 		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
-		err := priceJob.Process()
-		if err != nil {
-			fmt.Println("could not process job")
-			fmt.Println("error: ", err)
+		go priceJob.Process(doneChans[index], errorChans[index])
+	}
+	for index := range taxRates {
+		select {
+		case err := <-errorChans[index]:
+			if err != nil {
+				fmt.Println(err)
+			}
+		case <-doneChans[index]:
+			fmt.Println("Done!")
 		}
 	}
 }
